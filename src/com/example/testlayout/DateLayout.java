5bcb4bf988f941dd9b8c0e9580a1f65f4d6d6988
kuchiki111 <394957249@qq.com>
2017-03-19 19:57:33 +0800

    android study android layout practice
    Signed-off-by: kuchiki111

---
+++ src/com/example/testlayout/DateLayout.java
@@ -0,0 +1,13 @@
+package com.example.testlayout;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+public class DateLayout extends LinearLayout {
+
+	public DateLayout(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}

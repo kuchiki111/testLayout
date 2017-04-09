b71d9e4c8865bdc1c67d6f4d19651dc9397ce3fe
kuchiki111 <394957249@qq.com>
2017-04-09 12:59:03 +0800

    1.01

--- src/com/example/testlayout/DateLayout.java
+++ src/com/example/testlayout/DateLayout.java
@@ -4,54 +4,43 @@ import java.util.Calendar;
 import android.content.Context;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.text.Layout;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class DateLayout extends LinearLayout {
-		public void myAddView(){
+	
+		public void myAddView(int width,int height,Drawable background,int color,int textsize,String text){
+			LinearLayout LinearLayout = new LinearLayout(this.getContext());
+			TextView TextView = new TextView(this.getContext());
+			LinearLayout.LayoutParams layout ;
 			
+			LinearLayout.setGravity(Gravity.CENTER);
+			LinearLayout.setBackground(background);
+			
+			TextView.setTextColor(color);
+			TextView.setTextSize((int)getResources().getDimension(R.dimen.standard_dp)*textsize);
+			TextView.setText(text);
+			LinearLayout.addView(TextView);
+			
+			layout = new LinearLayout.LayoutParams(
+					(int)getResources().getDimension(R.dimen.standard_dp)*width,
+					(int)getResources().getDimension(R.dimen.standard_dp)*height);
+			this.addView(LinearLayout,layout);
 		}
+		
 		public DateLayout(Context context, AttributeSet attrs) {
 			super(context, attrs);
-			
 			this.setOrientation(VERTICAL);
-			this.setGravity(Gravity.CENTER);
-			LinearLayout.LayoutParams layout ;
+			
 			Calendar date = Calendar.getInstance();
+			String month = String.valueOf(date.get(Calendar.MONTH)+1+"ÔÂ");
+			String day = String.valueOf(date.get(Calendar.DAY_OF_MONTH));
 			
-			LinearLayout layoutTop = new LinearLayout(this.getContext());
-			layoutTop.setBackground(getResources().getDrawable(R.drawable.styletop));
-			layoutTop.setGravity(Gravity.CENTER);
-			
-			TextView month = new TextView(this.getContext());
-			month.setTextColor(Color.WHITE);
-			month.setTextSize(getResources().getDimension(R.dimen.standard_dp)*10);
-			month.setText(date.get(Calendar.MONTH)+1+"ÔÂ");
-			layoutTop.addView(month);
-			
-			layout = new LinearLayout.LayoutParams(
-					(int)getResources().getDimension(R.dimen.standard_dp)*80,
-					(int)getResources().getDimension(R.dimen.standard_dp)*30);
-			this.addView(layoutTop,layout);
-			
-			
-			LinearLayout layoutbottom = new LinearLayout(this.getContext());
-			layoutbottom.setBackground(getResources().getDrawable(R.drawable.stylebottom));
-			layoutbottom.setGravity(Gravity.CENTER);
-			
-			TextView day = new TextView(this.getContext());
-			day.setTextColor(Color.BLACK);
-			day.setTextSize(getResources().getDimension(R.dimen.standard_dp)*18);
-			day.setText(String.valueOf(date.get(Calendar.DAY_OF_MONTH)));
-			layoutbottom.addView(day);
-			
-			layout = new LinearLayout.LayoutParams(
-					(int)getResources().getDimension(R.dimen.standard_dp)*80,
-					(int)getResources().getDimension(R.dimen.standard_dp)*50);
-			layout.topMargin=-2;
-			this.addView(layoutbottom,layout);
-			
+			myAddView(80, 30, getResources().getDrawable(R.drawable.styletop), Color.WHITE, 10, month);
+			myAddView(80, 50, getResources().getDrawable(R.drawable.stylebottom), Color.BLACK, 18, day);
 		}
 }
